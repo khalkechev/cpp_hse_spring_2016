7aa9be8510c457dbb4574cb663e76e58e6c382cd
Maksim Tatarinov <maxim@aastra.su>
2016-03-19 18:00:40 +0300

    Create practice_04.cpp

---
+++ seminars/seminar_02/practice_04.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+int Strlen(const char* str) {
+    
+    int result = 0;
+    while (str[result] != '\0') {
+        ++result;
+    }
+    return result;
+    
+}
+
+
+
+int atoi(const char* str) {
+    
+    int result = 0;
+    int len = Strlen(str);
+    
+    for (int i = len - 1; i >= 0; --i){
+        
+        if (i == 0 && str[i] == '-') {
+            
+            result *= -1;
+            
+        } else {
+        
+        result += (str[i] - '0') * pow(10, len - 1 - i);
+        }
+    }
+    
+    return result;
+    
+}
+
+
+int main(int argc, char const *argv[]) {
+    
+    const char* str1 = "-103";
+    
+    std::cout << "str 1 + 1 = " << (atoi(str1) + 1) << std::endl;
+    
+    return 0;
+    
+}
