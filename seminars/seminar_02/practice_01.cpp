efaf25689346114c390b862c4471702da1bdd0ae
Maksim Tatarinov <maxim@aastra.su>
2016-03-21 19:47:50 +0300

    Update practice_01.cpp

--- seminars/seminar_02/practice_01.cpp
+++ seminars/seminar_02/practice_01.cpp
@@ -1,15 +1,11 @@
+
 #include <iostream>
 
-
-
-int main() {
-    
-    std::cout << "sizeofchar(char) = " << sizeof(char) << std::endl;
-    std::cout << "sizeofshort(short) = " << sizeof(short) << std::endl;
-    std::cout << "sizeofint(int) = " << sizeof(int) << std::endl;
-    
-    long x;
-    std::cout << "sizeofint(int) = " << sizeof(x) << std::endl;
+int main(int argc, char const *argv[]) {
+    if (sizeof(char) <= sizeof(int) && sizeof(int) <= sizeof(double)) {
+        std::cout << "Yes, it is.\n";
+    } else {
+        std::cout << "No, it is not.\n";
+    }
     return 0;
-    
 }
